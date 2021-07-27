68b45414138acdb56cb0d541735379b530488937
Gökberk Köksoy <gokberkkoksoy@icloud.com>
2021-07-27 22:54:24 +0300

    Implement table view

--- Assignment1/MainVC.swift
+++ Assignment1/MainVC.swift
@@ -10,36 +10,91 @@ import UIKit
 class MainVC: UIViewController {
     
     private let addItemTextField = GKTextField()
-
+    private let tableView = UITableView()
+    private var items = ["asdf","dsfg", "wey", "786854"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
-        view.addSubview(addItemTextField)
         configureTextField()
         createDismissKeyboardTapGesture()
-    }
-    
-    private func configureTextField() {
-        addItemTextField.delegate = self
-        
-        NSLayoutConstraint.activate([
-            addItemTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
-            addItemTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            addItemTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
-            addItemTextField.heightAnchor.constraint(equalToConstant: 45)
-        ])
+        configureTableView()
     }
     
     private func createDismissKeyboardTapGesture() {
         let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tap)
     }
+    
+    private func configureTextField() {
+        view.addSubview(addItemTextField)
+        addItemTextField.delegate = self
+        
+        NSLayoutConstraint.activate([
+            addItemTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            addItemTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            addItemTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            addItemTextField.heightAnchor.constraint(equalToConstant: 45)
+        ])
+    }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView(frame: .zero)
+        tableView.backgroundColor = .none
+        tableView.register(GKTableViewCell.self, forCellReuseIdentifier: GKTableViewCell.reuseID)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: addItemTextField.bottomAnchor, constant: 10),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    func addItem(item: String){
+        items.append(item)
+        DispatchQueue.main.async { self.tableView.reloadData() }
+    }
+    
 }
 
 extension MainVC: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.text != nil {
+            addItem(item: textField.text!)
+        }
         textField.resignFirstResponder()
         return true
+    }
+}
+
+extension MainVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath.section, animated: true)
+//    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: GKTableViewCell.reuseID) as! GKTableViewCell
+        cell.set(title: items[indexPath.row])
+        return cell
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        items.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
     }
 }
 
