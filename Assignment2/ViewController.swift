d192836512bf9ce32d75ec909b39351495745735
Gökberk Köksoy <gokberkkoksoy@icloud.com>
2021-07-28 17:27:41 +0300

    Initial Commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Gökberk Köksoy on 28.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
