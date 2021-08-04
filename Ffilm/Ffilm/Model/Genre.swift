356b2fa45ef99beed2b7b2630abc66487b9ade14
Gökberk Köksoy <gokberkkoksoy@icloud.com>
2021-08-04 09:20:23 +0300

    Data models are in separate files

---
+++ Ffilm/Ffilm/Model/Genre.swift
@@ -0,0 +1,12 @@
+//
+//  Genre.swift
+//  Ffilm
+//
+//  Created by Gökberk Köksoy on 4.08.2021.
+//
+
+import Foundation
+
+struct Genre: Codable {
+    var name: String?
+}
--- Ffilm/Ffilm/Model/Movie.swift
+++ Ffilm/Ffilm/Model/Movie.swift
@@ -20,35 +20,4 @@ struct Movie: Codable, Hashable {
     
 }
 
-struct MovieDetail: Codable {
-
-    var backdropPath: String?
-    var overview: String?
-    var releaseDate: String?
-    var genres: [Genre]?
-    var id: Int?
-    var title: String?
-    var runtime: Int?
-    var voteAverage: Double?
-    var tagline: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case backdropPath = "backdrop_path"
-        case releaseDate = "release_date"
-        case overview
-        case title
-        case id
-        case runtime
-        case voteAverage = "vote_average"
-        case tagline
-        case genres
-        
-    }
-    
-}
-
-struct Genre: Codable {
-    var name: String?
-}
-
 
